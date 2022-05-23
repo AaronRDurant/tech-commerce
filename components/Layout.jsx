6f3cd3739c39d2b13b77b58e48b576f3e36bcf39
Aaron Durant <aaron@aarondurant.com>
2022-05-23 16:19:40 -0400

    Build front page

---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Layout = () => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Tech Commerce</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Layout
