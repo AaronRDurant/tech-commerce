88f1a25f66355816b84e9f5ce79b609859a3b5bf
Aaron Durant <aaron@aarondurant.com>
2022-05-23 16:41:54 -0400

    Implement Layout.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer-container">
+      <p>Built by Aaron Durant in 2022</p>
+      <p className="icons">
+        <AiOutlineTwitter />
+        <AiOutlineLinkedin />
+        <AiOutlineGithub />
+      </p>
     </div>
   )
 }
